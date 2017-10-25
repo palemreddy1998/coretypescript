571e4f0a8e8dc20bf07a1b662056e5c49f81b65c
palemreddy <palemreddy1998@gmail.com>
2017-10-25 14:09:19 +0100

    Introduced typescript logging.

--- test/DataTypesTest.js
+++ test/DataTypesTest.js
@@ -3,8 +3,10 @@ exports.__esModule = true;
 /// <reference path="../typings/mocha/mocha.d.ts" />
 /// <reference path="../typings/chai/chai.d.ts"/>
 var chai_1 = require("chai");
+var Config_1 = require("../lib/Config");
 describe('Boolean test', function () {
     it('should test boolean operator', function () {
+        Config_1.log.info("Boolean test");
         var bool = false;
         chai_1.expect(bool).equals(false);
         bool = true;
@@ -13,12 +15,14 @@ describe('Boolean test', function () {
 });
 describe('Number test', function () {
     it('should test numbers', function () {
+        Config_1.log.info("Number test");
         var number1 = 500.45;
         chai_1.expect(number1).equals(500.45);
     });
 });
 describe('Date test', function () {
     it('should test date', function () {
+        Config_1.log.info("Date test");
         var date1 = new Date();
         console.log("date1 ->" + date1);
         chai_1.expect(date1).to.not.be["null"];
@@ -26,6 +30,7 @@ describe('Date test', function () {
 });
 describe('String test', function () {
     it('should test strings', function () {
+        Config_1.log.info("String test");
         var string1 = "mocha";
         chai_1.expect(string1).to.not.be["null"];
         chai_1.expect(string1).equals("mocha");
@@ -33,12 +38,30 @@ describe('String test', function () {
 });
 describe('Array test', function () {
     it('should test arrays', function () {
+        Config_1.log.info("Array test");
         var array1 = new Array('one', 'two', 'three', 'four', 'five');
         chai_1.expect(array1).length(5);
         //filter one value
         chai_1.expect(array1.filter(function (s) { return (s.indexOf('ne') > -1); })[0]).equals("one");
         //filter multiple values
         chai_1.expect(array1.filter(function (s) { return (s.indexOf('e') > -1); })).length(3);
+        array1[5] = 'six';
+        chai_1.expect(array1).length(6);
+    });
+});
+describe('Tuples test', function () {
+    it('should test tuples', function () {
+        Config_1.log.info("Tuples test");
+        var tuple1 = ['one', 'two', 'three', 'four', 'five'];
+        chai_1.expect(tuple1).length(5);
+        //filter one value
+        chai_1.expect(tuple1.filter(function (s) { return (s.indexOf('ne') > -1); })[0]).equals("one");
+        //filter multiple values
+        chai_1.expect(tuple1.filter(function (s) { return (s.indexOf('e') > -1); })).length(3);
+        tuple1[5] = 'six';
+        chai_1.expect(tuple1).length(6);
+        tuple1[0] = 'oneeeeeee';
+        chai_1.expect(tuple1).length(6);
     });
 });
 //# sourceMappingURL=DataTypesTest.js.map
--- test/DataTypesTest.ts
+++ test/DataTypesTest.ts
@@ -1,9 +1,11 @@
 /// <reference path="../typings/mocha/mocha.d.ts" />
 /// <reference path="../typings/chai/chai.d.ts"/>
 import { expect } from 'chai';
+import {log,catProd} from '../lib/Config';
 
 describe('Boolean test',()=>{
    it('should test boolean operator',()=>{
+       log.info("Boolean test");
        let bool:boolean = false;
        expect(bool).equals(false);
        bool=true;
@@ -14,6 +16,7 @@ describe('Boolean test',()=>{
 
 describe('Number test',()=>{
    it('should test numbers',()=>{
+       log.info("Number test");
        let number1:number = 500.45;
        expect(number1).equals(500.45)
    }) ;
@@ -21,6 +24,7 @@ describe('Number test',()=>{
 
 describe('Date test',()=>{
     it('should test date',()=>{
+        log.info("Date test");
         let date1:Date = new Date();
         console.log("date1 ->"+date1);
         expect(date1).to.not.be.null;
@@ -29,6 +33,7 @@ describe('Date test',()=>{
 
 describe('String test',()=>{
     it('should test strings',()=>{
+        log.info("String test");
         let string1:string = "mocha";
         expect(string1).to.not.be.null;
         expect(string1).equals("mocha");
@@ -37,12 +42,35 @@ describe('String test',()=>{
 
 describe('Array test',()=>{
     it('should test arrays',()=>{
+        log.info("Array test");
         let array1:Array<string> = new Array('one','two','three','four','five');
         expect(array1).length(5);
         //filter one value
         expect(array1.filter(s=>(s.indexOf('ne')>-1))[0]).equals("one");
         //filter multiple values
         expect(array1.filter(s=>(s.indexOf('e')>-1))).length(3)
+        array1[5]='six';
+        expect(array1).length(6);
     }) ;
 });
+
+
+
+
+describe('Tuples test',()=>{
+    it('should test tuples',()=>{
+        log.info("Tuples test");
+        let tuple1 = ['one','two','three','four','five'];
+        expect(tuple1).length(5);
+        //filter one value
+        expect(tuple1.filter(s=>(s.indexOf('ne')>-1))[0]).equals("one");
+        //filter multiple values
+        expect(tuple1.filter(s=>(s.indexOf('e')>-1))).length(3)
+        tuple1[5]='six';
+        expect(tuple1).length(6);
+        tuple1[0]='oneeeeeee';
+        expect(tuple1).length(6);
+    }) ;
+});
+
 
